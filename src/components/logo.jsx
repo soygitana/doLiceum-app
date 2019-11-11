7a1468b82edeef9cbccc82a65489e847717ac269
Lizon <v-Paula.Lizon@lionbridge.com>
2019-11-11 15:50:44 +0100

    settings

---
+++ src/components/logo.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+class Logo extends Component {
+    render() {
+        return (
+            <>
+       <img src="" alt=""/>
+          </>
+        )
+    }
+}
+
+export default Logo;
