c23ce359eedcddc168223edd0b991e385b0b8fec
Lizon <v-Paula.Lizon@lionbridge.com>
2019-11-14 12:32:37 +0100

    hero creation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,41 +1,51 @@
 import React, { Component } from "react";
 import Navigation from './navigation.jsx';
+// import Link from 'link-react';
 import home1 from '../assets/home1.jpg';
 import home2 from '../assets/home2.jpg';
-
 
 class Hero extends Component {
     render() {
         return (
             <>
                 <div className="app-container">
-                    <section className="Hero">
+                    <section className="hero">
 
                         <Navigation />
 
                         <div className="container-flex">
-
                             <div className="box-flex">
                                 <img className="img-fluid" src={home1} alt="yoga" />
-                                <h2 className="title">The best Yoga in the Town!</h2>
-
-
-
+                                <h2 className="title">The best Yoga in the Town</h2>
+                                <a className="button" href="#">Check classes</a>
                             </div>
-
-
-
-
-
                             <div className="box-flex">
                                 <img className="img-fluid" src={home2} alt="yoga" />
-                                <h2 className="title">Find your place in Namaste!</h2>
-
+                                <h2 className="title">Find your place in Namaste</h2>
+                                <a className="button" href="#">Check timetable</a>
                             </div>
-
-
                         </div>
-
+                        <div className="line"></div>
+                        <div className="container-flex">
+                            <div className="box-flex">
+                                <i className="fas fa-sun"></i>
+                                <h2 className="title">Find your balance</h2>
+                                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quam repellendus iusto laboriosam veniam iste aliquid accusantium animi facilis laborum tempore maiores sequi fugit.</p>
+                                <a className="button button-circle" href="#">More</a>
+                            </div>
+                            <div className="box-flex">
+                                <i className="fas fa-spa"></i>
+                                <h2 className="title">Inhale the future</h2>
+                                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quam repellendus iusto laboriosam veniam iste aliquid accusantium animi facilis laborum tempore maiores sequi fugit.</p>
+                                <a className="button button-circle" href="#">More</a>
+                            </div>
+                            <div className="box-flex">
+                                <i className="fas fa-certificate"></i>
+                                <h2 className="title">Stay strong</h2>
+                                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quam repellendus iusto laboriosam veniam iste aliquid accusantium animi facilis laborum tempore maiores sequi fugit.</p>
+                                <a className="button button-circle" href="#">More</a>
+                            </div>
+                        </div>
 
                     </section>
                 </div>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,32 +2,26 @@ import React, { Component } from "react";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Logo from '../components/logo.jsx'
 
-
 class Navigation extends Component {
     render() {
         return (
             <>
-
-
-<div className="app-container">     
-<nav className="page-nav">
-<Logo />
-<h1>Namaste</h1>
-<ul className="page-nav-list">
-<li><AnchorLink href='#'>Home</AnchorLink></li>
-<li><AnchorLink href='#'>Classes</AnchorLink></li>
-<li><AnchorLink href='#'>About</AnchorLink></li>
-<li><AnchorLink href='#'>Prices</AnchorLink></li>
-<li><AnchorLink href='#'>Contact</AnchorLink></li>
-</ul>
-</nav>
-</div>
-  </>   
+                    <nav className="page-nav">
+                        <Logo />
+                        <h1>Namaste</h1>
+                        <ul className="page-nav-list">
+                            <li><AnchorLink href='#'>Home</AnchorLink></li>
+                            <li><AnchorLink href='#'>Classes</AnchorLink></li>
+                            <li><AnchorLink href='#'>Timetable</AnchorLink></li>
+                            <li><AnchorLink href='#'>About</AnchorLink></li>
+                            <li><AnchorLink href='#'>Prices</AnchorLink></li>
+                            <li><AnchorLink href='#'>Contact</AnchorLink></li>
+                        </ul>
+                    </nav>
+            </>
         )
     }
 }
-
-
 
 export default Navigation;
 
